8a5598bb6b46d499fe89d839a5869ee1db246361
Chris Kottom <chris@chriskottom.com>
2011-01-18 14:30:46 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+
+gem "rails", "3.0.3"
+gem "rails3-generators"
+gem "mysql2"
+gem "haml"
+gem "haml-rails"
+gem "capistrano"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mashboard::Application.config.secret_token = 'e73e8d08d7a4d7a5d7163df2a8b5c132b7c47db4fedfb393e406537cf211db97888bbe5e62cd49630293d40207733edb65bfb86fea992aacf4f0c3c6a6dad3d6'
